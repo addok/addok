f3f741b8b34766eca6889c8f15897e45df7a9711
Yohan Boniface <yb@enix.org>
2014-12-29 23:15:14 +0100

    More stemmize tests

--- tests/textutils/test_fr.py
+++ tests/textutils/test_fr.py
@@ -13,6 +13,9 @@ from addok.textutils.fr import _stemmize
     ['wardrecques', 'vardrek'],
     ['cabourg', 'kabour'],
     ['audinghen', 'audingen'],
+    ['sault', 'sau'],
+    ['vaux', 'vau'],
+    ['guyancourt', 'guiankour'],
 ])
 def test_normalize(input, output):
     assert _stemmize(input) == output
