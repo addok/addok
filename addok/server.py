1a3dad7a981235be6f343503e96fff9efbc903ca
Yohan Boniface <yb@enix.org>
2015-01-06 19:48:54 +0100

    Add reverse view

--- addok/server.py
+++ addok/server.py
@@ -1,22 +1,61 @@
 import json
 
+from werkzeug.exceptions import HTTPException, BadRequest
+from werkzeug.routing import Map, Rule
 from werkzeug.wrappers import Request, Response
 
-from .core import search
+from .core import search, reverse
+
+url_map = Map([
+    Rule('/search/', endpoint='search'),
+    Rule('/reverse/', endpoint='reverse'),
+])
 
 
 def app(environ, start_response):
-    request = Request(environ)
+    urls = url_map.bind_to_environ(environ)
+    try:
+        endpoint, args = urls.match()
+    except HTTPException as e:
+        return e(environ, start_response)
+    else:
+        request = Request(environ)
+        if endpoint == 'search':
+            response = on_search(request)
+        elif endpoint == 'reverse':
+            response = on_reverse(request)
+    return response(environ, start_response)
+
+
+def on_search(request):
     query = request.args.get('q', '')
     try:
         limit = int(request.args.get('limit'))
-    except ValueError:
+    except (ValueError, TypeError):
         limit = 5
     try:
         autocomplete = int(request.args.get('autocomplete')) == '1'
     except (ValueError, TypeError):
         autocomplete = True
     results = search(query, limit=limit, autocomplete=autocomplete)
+    return serve_results(results)
+
+
+def on_reverse(request):
+    try:
+        lat = float(request.args.get('lat'))
+        lon = float(request.args.get('lon'))
+    except (ValueError, TypeError):
+        raise BadRequest()
+    try:
+        limit = int(request.args.get('limit'))
+    except (ValueError, TypeError):
+        limit = 1
+    results = reverse(lat=lat, lon=lon, limit=limit)
+    return serve_results(results)
+
+
+def serve_results(results):
     results = {
         "type": "FeatureCollection",
         "features": [r.to_geojson() for r in results]
@@ -24,4 +63,4 @@ def app(environ, start_response):
     response = Response(json.dumps(results), mimetype='text/plain')
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "X-Requested-With"
-    return response(environ, start_response)
+    return response
