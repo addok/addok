fe1b645fdfdf8da0ab24be6cfd7497001f46cfc5
Yohan Boniface <yb@enix.org>
2015-02-02 17:25:41 +0100

    Oops

--- addok/config/default.py
+++ addok/config/default.py
@@ -10,7 +10,7 @@ DB_SETTINGS = {
 BUCKET_LIMIT = 100
 
 # Above this treshold, terms are considered commons.
-COMMON_THRESHOLD = 500
+COMMON_THRESHOLD = 10000
 
 # Min score considered matching the query.
 MATCH_THRESHOLD = 0.9
