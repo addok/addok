f9c8054c5717a7a57bf0586e2a366439857b866e
Yohan Boniface <yb@enix.org>
2015-02-16 11:44:48 +0100

    Add deindex_document (fix #22)

--- addok/index_utils.py
+++ addok/index_utils.py
@@ -12,12 +12,27 @@ def index_edge_ngrams(pipe, token):
         pipe.sadd(edge_ngram_key(ngram), token)
 
 
+def deindex_edge_ngrams(token):
+    for ngram in compute_edge_ngrams(token):
+        DB.srem(edge_ngram_key(ngram), token)
+
+
 def index_field(pipe, key, string, boost=1.0, update_ngrams=True):
     els = list(preprocess(string))
     for s in els:
         pipe.zadd(token_key(s), 1.0 / len(els) * boost, key)
         if update_ngrams:
             index_edge_ngrams(pipe, s)
+    return els
+
+
+def deindex_field(key, string):
+    els = list(preprocess(string.decode()))
+    for s in els:
+        tkey = token_key(s)
+        DB.zrem(tkey, key)
+        if not DB.exists(tkey):
+            deindex_edge_ngrams(s)
     return els
 
 
@@ -30,6 +45,20 @@ def index_pairs(pipe, els):
                 values.add(el2)
         if values:
             pipe.sadd(pair_key(el), *values)
+
+
+def deindex_pairs(els):
+    els = set(els)  # Unique values.
+    for el in els:
+        for el2 in els:
+            if el != el2:
+                key = '|'.join(['didx', el, el2])
+                # Do we have other documents that share el and el2?
+                commons = DB.zinterstore(key, [token_key(el), token_key(el2)])
+                DB.delete(key)
+                if not commons:
+                    DB.srem(pair_key(el), el2)
+                    DB.srem(pair_key(el2), el)
 
 
 def index_document(doc, update_ngrams=True):
@@ -78,9 +107,30 @@ def index_document(doc, update_ngrams=True):
     pipe.execute()
 
 
+def deindex_document(id_):
+    key = document_key(id_)
+    doc = DB.hgetall(key)
+    if not doc:
+        return
+    DB.delete(key)
+    deindex_geohash(key, doc[b'lat'], doc[b'lon'])
+    pair_els = []
+    for name, value in doc.items():
+        pair_els.extend(deindex_field(key, value))
+    deindex_pairs(pair_els)
+
+
 def index_geohash(pipe, key, lat, lon):
     lat = float(lat)
     lon = float(lon)
     geoh = geohash.encode(lat, lon, config.GEOHASH_PRECISION)
     geok = geohash_key(geoh)
     pipe.sadd(geok, key)
+
+
+def deindex_geohash(key, lat, lon):
+    lat = float(lat)
+    lon = float(lon)
+    geoh = geohash.encode(lat, lon, config.GEOHASH_PRECISION)
+    geok = geohash_key(geoh)
+    DB.srem(geok, key)
--- tests/test_index_utils.py
+++ tests/test_index_utils.py
@@ -1,5 +1,6 @@
 from addok.core import DB
-from addok.index_utils import index_edge_ngrams, index_document
+from addok.index_utils import (index_edge_ngrams, index_document,
+                               deindex_document)
 
 
 def count_keys():
@@ -20,34 +21,121 @@ def test_index_edge_ngrams():
     assert DB.smembers('n|stree') == set([b'street'])
 
 
-def test_index_document():
-    doc = {
-        'id': 'xxxx',
-        'type': 'street',
-        'name': 'rue des Lilas',
-        'lat': '48.32545',
-        'lon': '2.2565',
-        'housenumbers': {
-            '1': {
-                'lat': '48.325451',
-                'lon': '2.25651'
-            }
+DOC = {
+    'id': 'xxxx',
+    'type': 'street',
+    'name': 'rue des Lilas',
+    'city': 'Andrésy',
+    'lat': '48.32545',
+    'lon': '2.2565',
+    'housenumbers': {
+        '1': {
+            'lat': '48.325451',
+            'lon': '2.25651'
         }
     }
-    index_document(doc)
+}
+
+
+def test_index_document():
+    index_document(DOC)
     assert DB.exists('d|xxxx')
     assert DB.type('d|xxxx') == b'hash'
     assert DB.exists('w|ru')
     assert b'd|xxxx' in DB.zrange('w|ru', 0, -1)
     assert DB.exists('w|de')
     assert DB.exists('w|lila')
+    assert DB.exists('w|andrezi')
     assert DB.exists('w|un')  # Housenumber.
     assert DB.exists('p|ru')
     assert DB.exists('p|de')
     assert DB.exists('p|lila')
+    assert DB.exists('p|andrezi')
+    assert b'lila' in DB.smembers('p|andrezi')
+    assert b'andrezi' in DB.smembers('p|lila')
     assert DB.exists('p|un')
     assert DB.exists('g|u09dgm7h')
     assert b'd|xxxx' in DB.smembers('g|u09dgm7h')
     assert DB.exists('n|lil')
+    assert DB.exists('n|and')
+    assert b'andrezi' in DB.smembers('n|and')
+    assert DB.exists('n|andr')
+    assert b'andrezi' in DB.smembers('n|andr')
+    assert DB.exists('n|andre')
+    assert b'andrezi' in DB.smembers('n|andre')
+    assert DB.exists('n|andrez')
+    assert b'andrezi' in DB.smembers('n|andrez')
     assert b'lila' in DB.smembers('n|lil')
-    assert len(DB.keys()) == 11
+    assert len(DB.keys()) == 17
+
+
+def test_deindex_document():
+    index_document(DOC)
+    deindex_document(DOC['id'])
+    assert not DB.exists('d|xxxx')
+    assert not DB.exists('w|de')
+    assert not DB.exists('w|lila')
+    assert not DB.exists('w|un')  # Housenumber.
+    assert not DB.exists('p|ru')
+    assert not DB.exists('p|de')
+    assert not DB.exists('p|lila')
+    assert not DB.exists('p|un')
+    assert not DB.exists('g|u09dgm7h')
+    assert not DB.exists('n|lil')
+    assert not DB.exists('n|and')
+    assert not DB.exists('n|andr')
+    assert not DB.exists('n|andre')
+    assert not DB.exists('n|andrez')
+    assert len(DB.keys()) == 0
+
+
+def test_deindex_document_should_not_affect_other_docs():
+    DOC2 = {
+        'id': 'xxxx2',
+        'type': 'street',
+        'name': 'rue des Lilas',
+        'city': 'Paris',
+        'lat': '49.32545',
+        'lon': '4.2565',
+        'housenumbers': {
+            '1': {
+                'lat': '48.325451',  # Same geohash as DOC.
+                'lon': '2.25651'
+            }
+        }
+    }
+    index_document(DOC)
+    index_document(DOC2)
+    deindex_document(DOC['id'])
+    assert not DB.exists('d|xxxx')
+    assert b'd|xxxx' not in DB.zrange('w|ru', 0, -1)
+    assert b'd|xxxx' not in DB.zrange('w|de', 0, -1)
+    assert b'd|xxxx' not in DB.zrange('w|lila', 0, -1)
+    assert b'd|xxxx' not in DB.zrange('w|un', 0, -1)
+    assert DB.exists('g|u09dgm7h')
+    assert b'd|xxxx' not in DB.smembers('g|u09dgm7h')
+    assert DB.exists('w|de')
+    assert DB.exists('w|lila')
+    assert DB.exists('w|un')  # Housenumber.
+    assert DB.exists('p|ru')
+    assert b'd|xxxx2' in DB.zrange('w|ru', 0, -1)
+    assert b'd|xxxx2' in DB.zrange('w|de', 0, -1)
+    assert b'd|xxxx2' in DB.zrange('w|lila', 0, -1)
+    assert b'd|xxxx2' in DB.zrange('w|un', 0, -1)
+    assert b'd|xxxx2' in DB.smembers('g|u09dgm7h')
+    assert b'd|xxxx2' in DB.smembers('g|u0g08g7m')
+    assert DB.exists('p|de')
+    assert DB.exists('p|lila')
+    assert DB.exists('p|un')
+    assert not DB.exists('n|and')
+    assert not DB.exists('n|andr')
+    assert not DB.exists('n|andre')
+    assert not DB.exists('n|andrez')
+    assert DB.exists('n|par')
+    assert DB.exists('n|lil')
+    assert b'lila' in DB.smembers('n|lil')
+    assert len(DB.keys()) == 15
+
+
+def test_deindex_document_should_not_fail_if_id_do_not_exist():
+    deindex_document('xxxxx')
