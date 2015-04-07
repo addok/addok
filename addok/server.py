338c5218c2f591da11c5d2cb894662b1f6ccbec0
Yohan Boniface <yb@enix.org>
2015-04-07 17:27:45 +0200

    Add BOM in CSV for making Excel happy (cf #43)

--- addok/server.py
+++ addok/server.py
@@ -1,3 +1,4 @@
+import codecs
 import csv
 import io
 import json
@@ -133,6 +134,7 @@ def on_csv(request):
             if key not in fieldnames:
                 fieldnames.append(key)
         output = io.StringIO()
+        output.write(codecs.BOM_UTF8.decode('utf-8'))
         writer = csv.DictWriter(output, fieldnames, dialect=dialect)
         writer.writeheader()
         for row in rows:
