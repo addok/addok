857cdacfa6a0401d48ae72f3634bcbe12ba15cd6
Yohan Boniface <yb@enix.org>
2015-08-28 10:41:31 +0200

    Allow to pass functions as PROCESSORS, instead of path

--- addok/utils.py
+++ addok/utils.py
@@ -16,6 +16,8 @@ def import_by_path(path):
     Import functions or class by their path. Should be of the form:
     path.to.module.func
     """
+    if not isinstance(path, str):
+        return path
     module_path, name = path.rsplit('.', 1)
     module = import_module(module_path)
     attr = getattr(module, name)
