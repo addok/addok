92000e47bf1b308179b4b00de81bd3c7eeddab98
Yohan Boniface <yb@enix.org>
2015-10-13 11:03:01 +0200

    Add command line parameter to set config module (cf #114)

--- addok/bin/__init__.py
+++ addok/bin/__init__.py
@@ -3,9 +3,9 @@
 Addok: search engine for address. Only address.
 Usage:
     addok serve [--port=<number>] [--host=<string>] [options]
-    addok shell
+    addok shell [options]
     addok batch [<filepath>...] [options]
-    addok ngrams
+    addok ngrams [options]
 
 Examples:
     addok serve --port 5432 --debug
@@ -25,21 +25,26 @@ Options:
     --dbhost=<string>   override dbhost
     --dbport=<string>   override dbport
     --limit=<number>    add an optional limit
+    --config=<path>     path to local config file
 """
 
+import os
 import sys
 
 from docopt import docopt
 
-from addok import config
-from addok.debug import Cli
-from addok.server import app
-from addok import batch
-from addok.index_utils import create_edge_ngrams
-
 
 def main():
     args = docopt(__doc__, version='Addok 0.1')
+
+    if args['--config']:
+        os.environ['ADDOK_CONFIG_MODULE'] = args['--config']
+
+    from addok import config
+    from addok.debug import Cli
+    from addok.server import app
+    from addok import batch
+    from addok.index_utils import create_edge_ngrams
 
     if args['serve']:
         from werkzeug.serving import run_simple
