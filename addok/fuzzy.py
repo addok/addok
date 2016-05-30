7356a0187c00a566a5920292a878e8d97f239c8d
Yohan Boniface <yb@enix.org>
2016-05-30 16:44:36 +0200

    Fix FUZZYINDEX command

--- addok/fuzzy.py
+++ addok/fuzzy.py
@@ -115,8 +115,8 @@ def do_fuzzyindex(self, word):
     FUZZYINDEX lilas"""
     word = list(preprocess_query(word))[0]
     token = Token(word)
-    token.make_fuzzy()
-    neighbors = [(n, DB.zcard(dbkeys.token_key(n))) for n in token.neighbors]
+    neighbors = make_fuzzy(token)
+    neighbors = [(n, DB.zcard(dbkeys.token_key(n))) for n in neighbors]
     neighbors.sort(key=lambda n: n[1], reverse=True)
     for token, freq in neighbors:
         if freq == 0:
