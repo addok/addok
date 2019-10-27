19812b40cab5cec2f880eebe96d262ad982755c7
cquest <cquest@openstreetmap.fr>
2019-10-27 10:42:57 +0100

    zstd compression support

--- addok/helpers/serializers.py
+++ addok/helpers/serializers.py
@@ -1,5 +1,7 @@
 import json
 import zlib
+import zstandard as zstd
+from pathlib import Path
 
 
 class ZlibSerializer:
@@ -11,3 +13,20 @@ class ZlibSerializer:
     @classmethod
     def loads(cls, data):
         return json.loads(zlib.decompress(data).decode())
+
+
+class ZstdSerializer:
+
+    dict_data = zstd.ZstdCompressionDict(
+        open(Path(__file__).parent / 'addok-zstd-dict', 'rb').read())
+    compressor = zstd.ZstdCompressor(level=10, dict_data=dict_data)
+    decompressor = zstd.ZstdDecompressor(dict_data=dict_data)
+
+
+    @classmethod
+    def dumps(cls, data):
+        return cls.compressor.compress(json.dumps(data).encode())
+
+    @classmethod
+    def loads(cls, data):
+        return json.loads(cls.decompressor.decompress(data).decode())
